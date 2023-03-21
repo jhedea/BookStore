c8a9bdeb981765dcbb40ff65764826ba436b1418
ioan-hedea <i.m.hedea@student.tudelft.nl>
2023-03-21 12:19:33 +0100

    Initial commit

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,13 @@
+namespace BookStore.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ BookStore/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BookStoreContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BookStoreContext") ?? 
+                      throw new InvalidOperationException("Connection string 'BookStoreContext' not found."
+                      )));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
