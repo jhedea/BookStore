0e82798b7713ec6047bd1a7a63656e0329cbac84
ioan-hedea <i.m.hedea@student.tudelft.nl>
2023-03-26 18:58:09 +0200

    update model for Book.cs class

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -1,14 +1,20 @@
 namespace BookStore.Models;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Book
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
     public string? Description { get; set; }
+    
+    [Column(TypeName = "decimal(18, 2)")]
+
     public decimal Price { get; set; }
 
     
